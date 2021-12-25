2c5d4399e6992c55442ce21a4c8ce5890ed9714f
yoyothesheep <yoyothesheep@gmail.com>
2021-12-25 21:37:17 +0000

    Add EventListener
    Add EventListener to catch & display mint info in FE.

--- src/App.js
+++ src/App.js
@@ -7,6 +7,9 @@ import GroupNFT from './utils/GroupNFT.json';
 // Constants
 const TWITTER_HANDLE = 'yoyothesheep';
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
+// TODO - update when deploy new contract
+const CONTRACT_ADDRESS = "0x0C330b5AdC5AE7f77ebc91dd99b7Da75e565e103";
+
 //const OPENSEA_LINK = '';
 //const TOTAL_MINT_COUNT = 50;
 
@@ -21,8 +24,13 @@ const App = () => {
     // Check access to Eth environment info
     const { ethereum } = window;
 
-    if (!ethereum) { console.log("Please install an Ethereum wallet before proceeding!")} 
-    else { console.log("Proceeding with this ethereum info:", ethereum)}
+    if (!ethereum) { 
+      console.log("Please install an Ethereum wallet before proceeding!");
+      return;
+    } 
+    else { 
+      console.log("Proceeding with this ethereum info:", ethereum);
+    }
 
     // Check authorization to user wallet
     const accounts = await ethereum.request({ method: 'eth_accounts' });
@@ -33,6 +41,10 @@ const App = () => {
       const thisAccount = accounts[0];
       console.log("Found an authorized account:", thisAccount);
       setCurrentAccount(thisAccount);
+
+      // set up listener for contract-emitted events.
+      // This is for users who ALREADY have their wallet connected & authorized.
+      setupEventListener();
     }
     else {
       console.log("TODO: checkIfWalletIsConnected: no authorized accounts. Need to authorize");
@@ -49,13 +61,46 @@ const App = () => {
         return;
       } 
       
+      // will cause MetaMask popup
       const accounts = await ethereum.request({ method: 'eth_requestAccounts' });
 
       // If multiple accounts authorized, use first one
       setCurrentAccount(accounts[0]);
       console.log("connectWallet: /n connected");
+
+      // set up listener for contract-emitted events.
+      // This is for users who ALREADY have their wallet connected & authorized.
+      setupEventListener();
+
     } catch (error) {
       console.log("connectWallet: /n", error);
+    }
+  };
+
+  // Set up listener
+  const setupEventListener = async () => {
+    try {
+      const { ethereum } = window;
+
+      // Same as below
+      if (ethereum) {
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const connectedContract = new ethers.Contract(CONTRACT_ADDRESS, GroupNFT.abi, signer);
+
+        // Listener setup
+        connectedContract.on("NewGroupNFTMinted", (from, tokenId) => {
+          console.log(from, tokenId.toNumber());
+          alert(`Hey there! We've minted your NFT and sent it to your wallet. It may be blank right now. It can take a max of 10 min to show up on OpenSea. Here's the link: https://testnets.opensea.io/assets/${CONTRACT_ADDRESS}/${tokenId.toNumber()}`)
+        });
+
+        console.log("Setup event listener!")
+    
+      } else {
+        console.log("askContractToMint: Ethereum object doesn't exist.");
+      }
+    } catch (error) {
+      console.log("askContractToMint: /n", error);
     }
   };
 
@@ -63,10 +108,6 @@ const App = () => {
   const askContractToMint = async () => {
 
     console.log("askContractToMint");
-
-    // Shouldn't this be globally declared on top? 
-    // TODO - update when deploy new contract
-    const CONTRACT_ADDRESS = "0xc6b67dA41FB3fEE450E931B90d8C30207c54FdaF";
 
     try {
       const { ethereum } = window;
@@ -85,7 +126,7 @@ const App = () => {
         console.log("mining.. wait..");
         await nftTxn.wait();
 
-        console.log(`Mined, see transaction: https://rinkeby.etherscan.io/tx/${nftTxn.hash}`);
+        console.log(`Mined & minted! See transaction: https://rinkeby.etherscan.io/tx/${nftTxn.hash}`);
 
       } else {
         console.log("askContractToMint: Ethereum object doesn't exist.");
@@ -103,15 +144,19 @@ const App = () => {
   );
 
   // Run these methods on pageload
-  useEffect(() => { checkIfWalletIsConnected(); }, []  );
+  useEffect(() => { 
+    checkIfWalletIsConnected(); 
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []  );
 
   return (
     <div className="App">
       <div className="container">
         <div className="header-container">
           <p className="header gradient-text">Roll the dice and get a Word Salad NFT</p>
+          <br/><br/>
           <p className="sub-text">
-            Come oooooon, discover your handle today.
+            Come oooooon, 👏🙌 do it ✊ do it 🤜👊🤛
           </p>
           {currentAccount === "" ? (
             renderNotConnectedContainer()
